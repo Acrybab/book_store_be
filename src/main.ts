4eb4da54b980055e7c9c94e9eb262ab6d46afb9a
Acrybab <khangtntce171659@fpt.edu.vn>
2025-12-08 15:03:53 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
 
   app.enableCors({
     origin: 'http://localhost:3000', // cho phép FE Next.js gọi
@@ -17,5 +16,6 @@ async function bootstrap() {
   app.useStaticAssets(join('D:\\LearnNestjs\\BookStore\\project-name\\uploads'), {
     prefix: '/uploads/',
   });
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
