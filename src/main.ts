37f3be4824fe80ac3b41c5b374a7f97866d0e5ef
Acrybab <khangtntce171659@fpt.edu.vn>
2025-12-14 16:54:54 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,17 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'https://bookshop-trong-khang.vercel.app', // cho phép FE Next.js gọi
+    origin: ['https://bookshop-trong-khang.vercel.app', 'http://localhost:3000'], // cho phép FE Next.js gọi
     credentials: true, // nếu bạn xài cookie/session
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
-  app.enableCors({
-    origin: 'http://localhost:3000', // cho phép FE Next.js gọi
-    credentials: true, // nếu bạn xài cookie/session
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  // app.enableCors({
+  //   origin: 'http://localhost:3000', // cho phép FE Next.js gọi
+  //   credentials: true, // nếu bạn xài cookie/session
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
   // const filePath = path.join('D:\\LearnNestjs\\BookStore\\project-name\\uploads', photoName);
 
   app.useStaticAssets(join('D:\\LearnNestjs\\BookStore\\project-name\\uploads'), {
