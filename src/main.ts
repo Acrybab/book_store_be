c7967777ae55f5481570f3269ad67684ce04f652
Acrybab <khangtntce171659@fpt.edu.vn>
2025-12-14 16:49:27 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,12 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+  app.enableCors({
+    origin: 'http://localhost:3000', // cho phép FE Next.js gọi
+    credentials: true, // nếu bạn xài cookie/session
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   // const filePath = path.join('D:\\LearnNestjs\\BookStore\\project-name\\uploads', photoName);
 
   app.useStaticAssets(join('D:\\LearnNestjs\\BookStore\\project-name\\uploads'), {
